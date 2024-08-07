393243ea3a46e47d062dce9fd4a131facbf196f9
Idan <holinikson@gmail.com>
2024-08-07 18:42:56 +0300

    update

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -24,19 +24,27 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
 
     return (
         <div className="text-gray-4 border-[1px] border-gray-3 bg-black-2 hover:bg-black-2 hover:shadow-xl shadow-black-2 rounded-lg p-4 duration-300">
-            {!imageLoaded ? (
-                <div className="flex flex-col space-y-3">
+            <div className="relative">
+                {!imageLoaded ? (
                     <Skeleton className="w-full h-56 rounded-lg" />
+                ) : (
+                    <Image
+                        width={800}
+                        height={600}
+                        src={project.imageUrl}
+                        alt={project.title}
+                        className="w-full h-56 object-cover rounded-lg"
+                    />
+                )}
+                <div className="absolute top-2  left-2 flex flex-wrap space-x-2">
+                    {project.tags.map((tag, index) => (
+                        <span key={index} className="inline-block bg-gray-3  rounded-full px-3 py-1 text-[12px] font-semibold  bg-opacity-70 text-white mr-2">
+                            #{tag}
+                        </span>
+                    ))}
                 </div>
-            ) : (
-                <Image
-                    width={800}
-                    height={600}
-                    src={project.imageUrl}
-                    alt={project.title}
-                    className="w-full h-56 object-cover rounded-lg"
-                />
-            )}
+            </div>
+
             <h2 className="text-2xl mt-4 text-white">{project.title}</h2>
 
             <div className="mt-4 flex flex-col justify-between space-y-4">
@@ -47,23 +55,16 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
                     {project.githubUrl && (
                         <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
                             <Button variant={"idan"}>
-                                <IoLogoGithub size={25} /> <span className="ml-2">View Code</span>
+                                <IoLogoGithub size={20} /> <span className="ml-2 text-xs">View Code</span>
                             </Button>
                         </a>
                     )}
                     <a href={project.livePreviewUrl} target="_blank" rel="noopener noreferrer">
                         <Button variant={"idan"}>
-                            <HiOutlineComputerDesktop className="animate-pulse text-red-500" size={25} /><span className="ml-2">Live Preview</span>
+                            <HiOutlineComputerDesktop className="animate-pulse text-red-500" size={20} /><span className="ml-2 text-xs">Live Preview</span>
                         </Button>
                     </a>
                 </div>
-            </div>
-            <div className="mt-4">
-                {project.tags.map((tag, index) => (
-                    <span key={index} className="inline-block rounded-full px-3 py-1 text-[12px] font-semibold text-gray-1 mr-2">
-                        #{tag}
-                    </span>
-                ))}
             </div>
         </div>
     );
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,7 +7,7 @@ const projectsArr: Project[] = [
     {
         title: "Devorah's Lane Website",
         description: "Developed a responsive and visually appealing website for 'Devorah's Lane,' a cosmetic business by Devorah Yaffe. This project utilized React for building a dynamic and interactive user interface, complemented by Tailwind CSS for custom, responsive design. The website features an engaging user experience perfectly aligned with the brand's identity, showcasing a booking system, a treatment results gallery, and a blog for beauty tips. The emphasis was on creating a frontend that is both aesthetically pleasing and functionally rich, enhancing the online presence of Devorah’s cosmetic services.",
-        tags: ["react", "tailwind-css", "frontend"],
+        tags: ["react", "website"],
         githubUrl: "https://github.com/idanshlomo1/website-devorahs-lane",
         imageUrl: "https://devorahslane.com/static/media/home-banner.061ba92dc2253a0ab1a6.jpg",
         livePreviewUrl: "https://devorahslane.com/",
@@ -16,7 +16,7 @@ const projectsArr: Project[] = [
     {
         title: "Idan Shlomo Website",
         description: "This website, designed using React and Tailwind CSS, showcases my graphic design and web development projects. It features an intuitive interface, a comprehensive project gallery, and a blog where I share industry insights. The site is fully responsive, ensuring a seamless experience on all devices, reflecting my expertise in creating visually appealing and technically robust digital solutions.",
-        tags: ["react", "tailwind-css", "frontend"],
+        tags: ["react", "website"],
         githubUrl: "https://github.com/idanshlomo1/business-website/tree/master/src",
         imageUrl: "https://idanso.netlify.app/static/media/logoImage.png",
         livePreviewUrl: "https://idanso.netlify.app/",
@@ -26,7 +26,7 @@ const projectsArr: Project[] = [
     {
         title: "Mali Dayan Landing page",
         description: "This landing page, crafted using React and Tailwind CSS, uniquely showcases Mali Dayan's marriage counseling services. It features a distinctive design, an engaging interface, and an interactive gallery that highlights the essence of her work. The site is fully responsive, providing a seamless experience on all devices and reflecting my capability to deliver customized and visually compelling digital solutions tailored to her professional needs",
-        tags: ["react", "tailwind-css", "frontend"],
+        tags: ["react", "landing page"],
         githubUrl: "https://github.com/idanshlomo1/landingpage-mali-dayan",
         imageUrl: "https://malidayan.com/static/media/logoImage.d339938cef7baa3cac14.jpeg",
         livePreviewUrl: "https://malidayan.com/",
@@ -36,7 +36,7 @@ const projectsArr: Project[] = [
     {
         title: "Moove Media Page",
         description: "This landing page, built using React and Tailwind CSS, uniquely showcases the services of Moove Media, a digital marketing agency specializing in business strategy and video editing. It features a distinctive design, an engaging interface, and an interactive gallery that highlights the essence of their work. The site is fully responsive, providing a seamless experience on all devices and reflecting my capability to deliver customized and visually compelling digital solutions tailored to their professional needs.",
-        tags: ["react", "tailwind-css", "frontend"],
+        tags: ["react", "landing page"],
         githubUrl: "https://github.com/idanshlomo1/landingpage-stav-smadar",
         imageUrl: "https://moovemedia.co.il/static/media/moovemedia_logo.7862736fda1d3306e7d0.png",
         livePreviewUrl: "https://moovemedia.co.il/",
